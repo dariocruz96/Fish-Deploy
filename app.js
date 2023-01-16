94ef3974ff9c710dae7a7559b6d91e056c345a83
dariocruz96 <dariocruz96@hotmail.com>
2023-01-16 21:28:57 +0000

    Created views (view, add, edit)

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.resolve(__dirname, "public"));
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/view", (req, res) => {
+  res.render("view");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.get("/edit", (req, res) => {
+  res.render("edit");
 });
 
 app.listen(port, () => {
