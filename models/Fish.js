45f57dd88230e8bf2d8d115010228b9adcb1eccc
dariocruz96 <dariocruz96@hotmail.com>
2023-01-19 14:04:18 +0000

    Added 1 field to models

--- models/Fish.js
+++ models/Fish.js
@@ -6,6 +6,7 @@ const fishSchema = new Schema(
     Number: Number,
     Name: String,
     Sell: Number,
+    "Spawn Rates": String,
     Where: String,
     Shadow: String,
     //"Total Catches to Unlock": String,
